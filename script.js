4cd4e3c53fbcb8c4f8bb71e20afce6d82392a61f
DipeshGoud <112858278+DipeshGoud@users.noreply.github.com>
2023-06-10 12:19:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+
+let dis = document.getElementById("display");
+
+let w = windows.innerWidth;
+let h = windows.innerHeight;
+
+dis.innerHtml = `Width: ${w} and Height: ${h}`;
